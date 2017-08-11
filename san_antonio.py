ac264a5cc721b6f5f0511f47cc42ca9e64c2dc65
davidgillard <david.gillard@ac-lille.fr>
2017-08-12 00:20:52 +0200

    creation des fonctions read_key_from_json et random_quote

--- san_antonio.py
+++ san_antonio.py
@@ -3,7 +3,7 @@
 import random
 import json
 
-quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !","On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
+#quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !","On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
 
 def read_value_from_json():
     values = []
@@ -13,6 +13,14 @@ def read_value_from_json():
             values.append(entry['character'])
         return values
 
+def read_key_from_json():
+    key = []
+    with open('quotes.json') as f:
+        data = json.load(f)
+        for entry in data:
+            values.append(entry['quote'])
+        return key
+
 def get_random_item(my_list):
     rand_numb = random.randint(0, len(my_list) - 1)
     item = my_list[rand_numb]
@@ -21,6 +29,10 @@ def get_random_item(my_list):
 def random_character():
     all_values = read_value_from_json()
     return get_random_item(all_values)
+
+def random_quote():
+    all_key = read_key_from_json()
+    return get_random_item(all_key)
 
 def capitalize(words):
     for word in words:
@@ -34,5 +46,5 @@ def message(character, quote):
 user_answer = input('Tapez entrée pour connaitre une autre citation ou B pour quitter le programme.')
 
 while user_answer != "B":
-    print(message(random_character(), get_random_item(quotes)))
+    print(message(random_character(), random_quote()))
     user_answer = input('Tapez entrée pour connaitre une autre citation ou B pour quitter le programme.')
